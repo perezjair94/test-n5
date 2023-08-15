b941c58af5a393d4e3f4672f543a5f5765b663f1
Jair Pérez <oquendojair10@gmail.com>
2023-08-15 12:56:39 -0500

    feat: create cart page

---
+++ src/pages/cart.tsx
@@ -0,0 +1,39 @@
+import { Icon } from "@/components/Icon";
+import Layout from "@/components/Layout";
+import ProductItemCard from "@/components/ProductItemCard";
+import { useCart } from "@/context/cart.context";
+import styles from "@/styles/Cart.module.scss";
+
+export default function Cart() {
+  const { cartItems, totalPrice, onBuyItems } = useCart();
+  return (
+    <Layout>
+      <main className={styles.container}>
+        <div className={styles.grid}>
+          <div>
+            <h2>Carrito de compras</h2>
+            {!!cartItems.length ? (
+              cartItems.map((product) => (
+                <ProductItemCard key={product.id} {...product} />
+              ))
+            ) : (
+              <div>
+                <Icon name="HardDrive" size={20} />
+                <span>Sin datos</span>
+              </div>
+            )}
+          </div>
+          {!!cartItems.length && (
+            <div>
+              <h2>Resumen</h2>
+              <div>
+                <h3>Total $ {totalPrice}</h3>
+                <button onClick={onBuyItems}>Finalizar compra</button>
+              </div>
+            </div>
+          )}
+        </div>
+      </main>
+    </Layout>
+  );
+}
