a0e3ffe72fa3741f95081d7aeae94c33730bf778
Jair Pérez <oquendojair10@gmail.com>
2023-08-15 12:51:38 -0500

    feat: create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { useCart } from "@/context/cart.context";
+import { Icon } from "./Icon";
+import ToggleThemeButton from "./ToggleThemeButton";
+import styles from "@/styles/Header.module.scss";
+import Link from "next/link";
+
+export default function Header() {
+  const { cartItems } = useCart();
+  const cartItemsLength = cartItems.length || null;
+  return (
+    <header>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Link href="/">
+            <h2>Test N5</h2>
+          </Link>
+          <div>
+            <Link href="/cart">
+              {cartItemsLength}
+              <Icon name="ShoppingCart" />
+            </Link>
+            <ToggleThemeButton />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
