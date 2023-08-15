2c5dbcb3dc850162bcc07498bd7714abd8b49232
Jair Pérez <oquendojair10@gmail.com>
2023-08-15 12:56:08 -0500

    feat(_app): add swr config and cart provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
+import CartProvider from "@/context/cart.context";
+import fetcher from "@/libs/fetcher";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CartProvider>
+      <SWRConfig
+        value={{ fetcher: (url, ...args) => fetcher({ url, ...args }) }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
+    </CartProvider>
+  );
 }
